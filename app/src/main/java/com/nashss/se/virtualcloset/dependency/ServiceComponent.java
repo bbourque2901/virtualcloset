6f4aaef0b9b5a95268454763df58f0544fe2df2e
Breanna <bourquebreanna@gmail.com>
2024-05-23 08:41:38 -0500

    updated GetOutfitLambda, created DaoModule class, LambdaActivityRunner class, ServiceComponent interface, and MetricsModule class

---
+++ app/src/main/java/com/nashss/se/virtualcloset/dependency/ServiceComponent.java
@@ -0,0 +1,17 @@
+package com.nashss.se.virtualcloset.dependency;
+
+import com.nashss.se.virtualcloset.activity.GetOutfitActivity;
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Component(modules = {DaoModule.class, MetricsModule.class})
+public interface ServiceComponent {
+
+    /**
+     * Provides the relevant activity.
+     * @return GetOutfitActivity
+     */
+    GetOutfitActivity provideGetOutfitActivity();
+}
--- app/src/main/java/com/nashss/se/virtualcloset/lambda/GetOutfitLambda.java
+++ app/src/main/java/com/nashss/se/virtualcloset/lambda/GetOutfitLambda.java
@@ -2,8 +2,12 @@ package com.nashss.se.virtualcloset.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.virtualcloset.activity.requests.GetOutfitRequest;
+import com.nashss.se.virtualcloset.activity.results.GetOutfitResult;
 
-public class GetOutfitLambda implements RequestHandler<LambdaRequest<LambdaRequest>, LambdaResponse> {
+public class GetOutfitLambda
+        extends LambdaActivityRunner<GetOutfitRequest, GetOutfitResult>
+        implements RequestHandler<LambdaRequest<GetOutfitRequest>, LambdaResponse> {
     /**
      * Handles a Lambda Function request.
      *
@@ -12,7 +16,7 @@ public class GetOutfitLambda implements RequestHandler<LambdaRequest<LambdaReque
      * @return The Lambda Function output
      */
     @Override
-    public LambdaResponse handleRequest(LambdaRequest<LambdaRequest> input, Context context) {
+    public LambdaResponse handleRequest(LambdaRequest<GetOutfitRequest> input, Context context) {
         return LambdaResponse.success();
     }
 }
