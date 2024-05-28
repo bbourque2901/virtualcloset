54ae8859a2e10ad49660e29b1ef1fe99b8857611
Breanna <bourquebreanna@gmail.com>
2024-05-28 17:32:48 -0500

    added virtualClosetClient.js, updated header.js, added viewOutfit.js, added outfit.html, and updated style.css

---
+++ web/src/api/virtualClosetClient.js
@@ -0,0 +1,119 @@
+import axios from "axios";
+import BindingClass from "../util/bindingClass";
+import Authenticator from "./authenticator";
+
+/**
+ * Client to call the VirtualCloset.
+ */
+
+export default class virtualClosetClient extends BindingClass {
+
+    constructor(props = {}) {
+        super();
+
+        const methodsToBind = ['clientLoaded', 'getIdentity', 'login', 'logout', 'getOutfit', 'createOutfit'];
+        this.bindClassMethods(methodsToBind, this);
+
+        this.authenticator = new Authenticator();;
+        this.props = props;
+
+        axios.defaults.baseURL = process.env.API_BASE_URL;
+        this.axiosClient = axios;
+        this.clientLoaded();
+    }
+
+    clientLoaded() {
+        if (this.props.hasOwnProperty("onReady")) {
+            this.props.onReady(this);
+        }
+    }
+
+    async getIdentity(errorCallback) {
+        try {
+            const isLoggedIn = await this.authenticator.isUserLoggedIn();
+
+            if (!isLoggedIn) {
+                return undefined;
+            }
+
+            return await this.authenticator.getCurrentUserInfo();
+        } catch (error) {
+            this.handleError(error, errorCallback)
+        }
+    }
+
+    async login() {
+        this.authenticator.login();
+    }
+
+    async logout() {
+        this.authenticator.logout();
+    }
+
+    async getTokenOrThrow(unauthenticatedErrorMessage) {
+        const isLoggedIn = await this.authenticator.isUserLoggedIn();
+        if (!isLoggedIn) {
+            throw new Error(unauthenticatedErrorMessage);
+        }
+
+        return await this.authenticator.getUserToken();
+    }
+
+    /**
+     * Gets the outfit for the given ID.
+     * @param id Unique identifier for an outfit
+     * @param errorCallback (Optional) A function to execute if the call fails.
+     * @returns The outfit's metadata.
+     */
+    async getOutfit(id, errorCallback) {
+        try {
+            const response = await this.axiosClient.get(`outfits/${id}`);
+            return response.data.outfit;
+        } catch (error) {
+            this.handleError(error, errorCallback)
+        }
+    }
+
+    /**
+     * Create a new outfit owned by the current user.
+     * @param name The name of the outfit to create.
+     * @param tags Metadata tags to associate with an outfit.
+     * @param errorCallback (Optional) A function to execute if the call fails.
+     * @returns The outfit that has been created.
+     */
+    async createOutfit(name, tags, errorCallback) {
+        try {
+            const token = await this.getTokenOrThrow("Only authenticated users can create outfits.");
+            const response = await this.axiosClient.post(`outfits`, {
+                name: name,
+                tags: tags
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data.outfit;
+        } catch (error) {
+            this.handleError(error, errorCallback)
+        }
+    }
+
+    /**
+     * Helper method to log the error and run any error functions.
+     * @param error The error received from the server.
+     * @param errorCallback (Optional) A function to execute if the call fails.
+     */
+    handleError(error, errorCallback) {
+        console.error(error);
+
+        const errorFromApi = error?.response?.data?.error_message;
+        if (errorFromApi) {
+            console.error(errorFromApi)
+            error.message = errorFromApi;
+        }
+
+        if (errorCallback) {
+            errorCallback(error);
+        }
+    }
+}
---
+++ web/src/pages/viewOutfit.js
@@ -0,0 +1,72 @@
+import VirtualClosetClient from '../api/virtualClosetClient';
+import Header from '../components/header';
+import BindingClass from "../util/bindingClass";
+import DataStore from "../util/DataStore";
+
+/**
+ * Logic needed for the view outfit page of the website.
+ */
+class ViewOutfit extends BindingClass {
+    constructor() {
+        super();
+        this.bindClassMethods(['clientLoaded', 'mount', 'addOutfitToPage'], this);
+        this.dataStore = new DataStore();
+        this.dataStore.addChangeListener(this.addOutfitToPage);
+        this.header = new Header(this.dataStore);
+        console.log("viewoutfit constructor");
+    }
+
+ /**
+     * Once the client is loaded, get the outfit metadata and clothing list.
+     */
+ async clientLoaded() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const outfitId = urlParams.get('id');
+    document.getElementById('outfit-name').innerText = "Loading Outfit ...";
+    const outfit = await this.client.getOutfit(outfitId);
+    this.dataStore.set('outfit', outfit);
+    //document.getElementById('clothing').innerText = "(loading clothing...)";
+   // const clothing = await this.client.getOutfitClothes(outfitId);
+    //this.dataStore.set('clothing', clothing);
+}
+
+mount() {
+    document.getElementById('add-clothing').addEventListener('click', this.addClothing);
+
+    this.header.addHeaderToPage();
+
+    this.client = new VirtualClosetClient();
+    this.clientLoaded();
+}
+
+/**
+     * When the outfit is updated in the datastore, update the outfit metadata on the page.
+     */
+addOutfitToPage() {
+    const outfit = this.dataStore.get('outfit');
+    if (outfit == null) {
+        return;
+    }
+
+    document.getElementById('outfit-name').innerText = outfit.name;
+    //document.getElementById('outfit-owner').innerText = outfit.customerName;
+
+    let tagHtml = '';
+    let tag;
+    for (tag of outfit.tags) {
+        tagHtml += '<div class="tag">' + tag + '</div>';
+    }
+    document.getElementById('tags').innerHTML = tagHtml;
+}
+
+}
+
+/**
+ * Main method to run when the page contents have loaded.
+ */
+const main = async () => {
+    const viewOutfit = new ViewOutfit();
+    viewOutfit.mount();
+};
+
+window.addEventListener('DOMContentLoaded', main);
