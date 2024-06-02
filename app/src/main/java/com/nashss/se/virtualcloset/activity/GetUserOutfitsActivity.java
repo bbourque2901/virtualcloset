cc93f0a29783b79237046c7cfc7d637d4a8d2388
Breanna <bourquebreanna@gmail.com>
2024-06-01 23:09:09 -0500

    added GetUserOutfits activity, request, and result classes, added getAllOutfitsForUser method to OutfitDao. added UserNotFoundException

---
+++ app/src/main/java/com/nashss/se/virtualcloset/activity/GetUserOutfitsActivity.java
@@ -0,0 +1,50 @@
+package com.nashss.se.virtualcloset.activity;
+
+import com.nashss.se.virtualcloset.activity.requests.GetUserOutfitsRequest;
+import com.nashss.se.virtualcloset.activity.results.GetUserOutfitsResult;
+import com.nashss.se.virtualcloset.converters.ModelConverter;
+import com.nashss.se.virtualcloset.dynamodb.Outfit;
+import com.nashss.se.virtualcloset.dynamodb.OutfitDao;
+import com.nashss.se.virtualcloset.models.OutfitModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class GetUserOutfitsActivity {
+
+    private final Logger log = LogManager.getLogger();
+    private final OutfitDao outfitDao;
+
+    /**
+     * Instantiates a new GetUserOutfitsActivity object.
+     *
+     * @param outfitDao OutfitDao to access the outfit table.
+     */
+    @Inject
+    public GetUserOutfitsActivity(OutfitDao outfitDao) {
+        this.outfitDao = outfitDao;
+    }
+
+    /**
+     * This method handles the incoming request by retrieving the user's outfits from the database.
+     * It then returns a list of outfits.
+     * If the user does not exist, this should throw a UserNotFoundException.
+     *
+     * @param getUserOutfitsRequest request object containing the customer ID
+     * @return getUserOutfitsResult result object containing the API defined {@link OutfitModel}
+     */
+    public GetUserOutfitsResult handleRequest(final GetUserOutfitsRequest getUserOutfitsRequest) {
+        log.info("Received GetUserOutfitsRequest {}", getUserOutfitsRequest);
+
+        String customerId =  getUserOutfitsRequest.getCustomerId();
+
+        List<Outfit> usersOutfits = outfitDao.getAllOutfitsForUser(customerId);
+        List<OutfitModel> usersOutfitModels = new ModelConverter().toOutfitModelList(usersOutfits);
+
+        return GetUserOutfitsResult.builder()
+                .withOutfits(usersOutfitModels)
+                .build();
+    }
+}
--- app/src/main/java/com/nashss/se/virtualcloset/dynamodb/OutfitDao.java
+++ app/src/main/java/com/nashss/se/virtualcloset/dynamodb/OutfitDao.java
@@ -1,13 +1,20 @@
 package com.nashss.se.virtualcloset.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.nashss.se.virtualcloset.exceptions.OutfitNotFoundException;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
+import com.nashss.se.virtualcloset.exceptions.UserNotFoundException;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Accesses data for an outfit using {@link Outfit} to represent the model in DynamoDB.
@@ -61,6 +68,27 @@ public class OutfitDao {
     public Outfit saveOutfit(Outfit outfit) {
         this.dynamoDBMapper.save(outfit);
         return outfit;
+    }
+
+    /**
+     * Searches outfit table for outfits with a matching customerId.
+     * @param customerId the customerId being searched
+     * @return list of outfit objects w/ customerId
+     */
+    public List<Outfit> getAllOutfitsForUser(String customerId) {
+        DynamoDBScanExpression dynamoDBScanExpression = new DynamoDBScanExpression();
+
+        if (customerId == null) {
+            throw new UserNotFoundException("That user doesn't exist yet!");
+        }
+
+        Map<String, AttributeValue> valueMap = new HashMap<>();
+        valueMap.put(":customerId", new AttributeValue().withS(customerId));
+
+        dynamoDBScanExpression.setExpressionAttributeValues(valueMap);
+        dynamoDBScanExpression.setFilterExpression("customerId = :customerId");
+
+        return this.dynamoDBMapper.scan(Outfit.class, dynamoDBScanExpression);
     }
 }
 
