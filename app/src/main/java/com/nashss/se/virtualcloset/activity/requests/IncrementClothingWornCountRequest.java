981ad0f049c98af106a5b9e572e9c9d34802e022
Breanna <bourquebreanna@gmail.com>
2024-06-09 14:03:31 -0500

    added result and request classes

---
+++ app/src/main/java/com/nashss/se/virtualcloset/activity/requests/IncrementClothingWornCountRequest.java
@@ -0,0 +1,38 @@
+package com.nashss.se.virtualcloset.activity.requests;
+
+public class IncrementClothingWornCountRequest {
+    private final String clothingId;
+
+    private IncrementClothingWornCountRequest(String clothingId) {
+        this.clothingId = clothingId;
+    }
+
+    public String getClothingId() {
+        return clothingId;
+    }
+
+    @Override
+    public String toString() {
+        return "IncrementClothingWornCountRequest{" +
+                "clothingId='" + clothingId + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String clothingId;
+
+        public Builder withCustomerId(String clothingId) {
+            this.clothingId = clothingId;
+            return this;
+        }
+
+        public IncrementClothingWornCountRequest build() {
+            return new IncrementClothingWornCountRequest(clothingId);
+        }
+    }
+}
