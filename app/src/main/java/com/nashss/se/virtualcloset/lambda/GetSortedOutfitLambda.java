e64e05e0cd3d1ba4816089fe88a9d2e29dac4c64
Breanna <bourquebreanna@gmail.com>
2024-06-11 10:42:25 -0500

    updated lambda classes to use authentication & get customerId from correct place

--- app/src/main/java/com/nashss/se/virtualcloset/lambda/GetSortedOutfitLambda.java
+++ app/src/main/java/com/nashss/se/virtualcloset/lambda/GetSortedOutfitLambda.java
@@ -1,5 +1,6 @@
 package com.nashss.se.virtualcloset.lambda;
 
+import com.nashss.se.virtualcloset.activity.requests.GetSortedClothingRequest;
 import com.nashss.se.virtualcloset.activity.requests.GetSortedOutfitRequest;
 import com.nashss.se.virtualcloset.activity.results.GetSortedOutfitResult;
 
@@ -8,17 +9,24 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class GetSortedOutfitLambda
         extends LambdaActivityRunner<GetSortedOutfitRequest, GetSortedOutfitResult>
-        implements RequestHandler<LambdaRequest<GetSortedOutfitRequest>, LambdaResponse> {
+        implements RequestHandler<AuthenticatedLambdaRequest<GetSortedOutfitRequest>, LambdaResponse> {
     @Override
-    public LambdaResponse handleRequest(LambdaRequest<GetSortedOutfitRequest> input, Context context) {
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<GetSortedOutfitRequest> input, Context context) {
         return super.runActivity(
-            () -> input.fromQuery(query ->
-                    GetSortedOutfitRequest.builder()
-                            .withCustomerId(query.get("customerId"))
-                            .withAscending(Boolean.parseBoolean(query.get("ascending")))
-                            .build()),
-            (request, serviceComponent) ->
-                    serviceComponent.provideGetSortedOutfitActivity().handleRequest(request)
+                () -> {
+                    GetSortedOutfitRequest unAuthRequest = input.fromUserClaims(claims ->
+                            GetSortedOutfitRequest.builder()
+                                    .withCustomerId(claims.get("email"))
+                                    .build());
+
+                    return input.fromQuery(query ->
+                            GetSortedOutfitRequest.builder()
+                                    .withCustomerId(unAuthRequest.getCustomerId())
+                                    .withAscending(Boolean.parseBoolean(query.getOrDefault("ascending", "true")))
+                                    .build());
+                },
+                (request, serviceComponent) ->
+                        serviceComponent.provideGetSortedOutfitActivity().handleRequest(request)
         );
     }
 }
