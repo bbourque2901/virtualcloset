9e6f4249bfcb6af345e6ed79d67becb044a712bf
Breanna <bourquebreanna@gmail.com>
2024-06-14 18:25:51 -0500

    updated so that when you increment an outfits worncount it also updates the worncount of the clothing items in the outfit

--- app/src/main/java/com/nashss/se/virtualcloset/activity/IncrementOutfitWornCountActivity.java
+++ app/src/main/java/com/nashss/se/virtualcloset/activity/IncrementOutfitWornCountActivity.java
@@ -3,6 +3,8 @@ package com.nashss.se.virtualcloset.activity;
 import com.nashss.se.virtualcloset.activity.requests.IncrementOutfitWornCountRequest;
 import com.nashss.se.virtualcloset.activity.results.IncrementOutfitWornCountResult;
 import com.nashss.se.virtualcloset.converters.ModelConverter;
+import com.nashss.se.virtualcloset.dynamodb.Clothing;
+import com.nashss.se.virtualcloset.dynamodb.ClothingDao;
 import com.nashss.se.virtualcloset.dynamodb.Outfit;
 import com.nashss.se.virtualcloset.dynamodb.OutfitDao;
 import com.nashss.se.virtualcloset.models.OutfitModel;
@@ -10,19 +12,23 @@ import com.nashss.se.virtualcloset.models.OutfitModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import javax.inject.Inject;
 
 public class IncrementOutfitWornCountActivity {
     private final Logger log = LogManager.getLogger();
     private final OutfitDao outfitDao;
+    private final ClothingDao clothingDao;
 
     /**
      * Instantiates new IncrementOutfitWornCountActivity object.
      * @param outfitDao outfitDao accesses the outfit table
+     * @param clothingDao clothingDao accesses the clothing table
      */
     @Inject
-    public IncrementOutfitWornCountActivity(OutfitDao outfitDao) {
+    public IncrementOutfitWornCountActivity(OutfitDao outfitDao, ClothingDao clothingDao) {
         this.outfitDao = outfitDao;
+        this.clothingDao = clothingDao;
     }
 
     /**
@@ -38,6 +44,17 @@ public class IncrementOutfitWornCountActivity {
 
         outfit.setWornCount(outfit.getWornCount() + 1);
         outfitDao.saveOutfit(outfit);
+
+        //also increment worn count for clothing items in the outfit
+        List<Clothing> clothingItems = outfit.getClothingItems();
+        if (clothingItems != null && !clothingItems.isEmpty()) {
+            for (Clothing clothing : clothingItems) {
+                clothing.setWornCount(clothing.getWornCount() + 1);
+                clothingDao.saveClothing(clothing);
+            }
+        } else {
+            log.info("No clothing items are in this outfit yet");
+        }
 
         OutfitModel outfitModel = new ModelConverter().toOutfitModel(outfit);
 
