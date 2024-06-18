832ad64fdcd0775c2a62f6c09423cb88795c8c27
Breanna <bourquebreanna@gmail.com>
2024-06-17 22:25:34 -0500

    fixing checkstyles

--- app/src/main/java/com/nashss/se/virtualcloset/lambda/DeleteClothingLambda.java
+++ app/src/main/java/com/nashss/se/virtualcloset/lambda/DeleteClothingLambda.java
@@ -1,9 +1,10 @@
 package com.nashss.se.virtualcloset.lambda;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.nashss.se.virtualcloset.activity.requests.DeleteClothingRequest;
 import com.nashss.se.virtualcloset.activity.results.DeleteClothingResult;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class DeleteClothingLambda
         extends LambdaActivityRunner<DeleteClothingRequest, DeleteClothingResult>
@@ -11,19 +12,19 @@ public class DeleteClothingLambda
     @Override
     public LambdaResponse handleRequest(AuthenticatedLambdaRequest<DeleteClothingRequest> input, Context context) {
         return super.runActivity(
-                () -> {
-                    DeleteClothingRequest unAuthenticatedRequest = input.fromUserClaims(claims ->
-                            DeleteClothingRequest.builder()
-                                    .withCustomerId(claims.get("email"))
-                                    .build());
-                    return input.fromPath(path ->
-                            DeleteClothingRequest.builder()
-                                    .withClothingId(path.get("clothingId"))
-                                    .withCustomerId(unAuthenticatedRequest.getCustomerId())
-                                    .build());
-                },
-                (request, serviceComponent) ->
-                        serviceComponent.provideDeleteClothingActivity().handleRequest(request)
+            () -> {
+                DeleteClothingRequest unAuthenticatedRequest = input.fromUserClaims(claims ->
+                        DeleteClothingRequest.builder()
+                                 .withCustomerId(claims.get("email"))
+                                 .build());
+                return input.fromPath(path ->
+                        DeleteClothingRequest.builder()
+                                .withClothingId(path.get("clothingId"))
+                                .withCustomerId(unAuthenticatedRequest.getCustomerId())
+                                .build());
+            },
+            (request, serviceComponent) ->
+                    serviceComponent.provideDeleteClothingActivity().handleRequest(request)
         );
     }
 }
